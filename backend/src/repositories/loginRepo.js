60f6df3b4628eee8c97b6d650f39fd92b82c763b
kisborg <mark.borgstrom@gmail.com>
2020-10-14 11:18:14 +0200

    formatted loginRepo.js

--- backend/src/repositories/loginRepo.js
+++ backend/src/repositories/loginRepo.js
@@ -1,12 +1,12 @@
 import { db } from '../data/connection';
 
 export const loginRepo = {
-    async getUser(username) {
-      try {
-        const sql = 'SELECT * FROM users WHERE username = ?';
-        return await db.query(sql, username);
-      } catch(err) {
-          throw {status: 500, message: 'Internal server error'}
-      }
+  async getUser(username) {
+    try {
+      const sql = 'SELECT * FROM users WHERE username = ?';
+      return await db.query(sql, username);
+    } catch(err) {
+        throw {status: 500, message: 'Internal server error'}
     }
+  }
 }
