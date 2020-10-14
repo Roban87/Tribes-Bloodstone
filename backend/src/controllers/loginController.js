fae8ddc42bbc507e558ce39c8924932e21b1bb22
Márk Borgström <64068936+kisborg@users.noreply.github.com>
2020-10-14 09:27:52 +0200

    Update backend/src/controllers/loginController.js
    Co-authored-by: Cserven Máté <38046719+matecserven@users.noreply.github.com>

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -5,7 +5,7 @@ export const loginController = {
     const { password, username } = req.body;
     try {
      const token = await loginService.getToken(username, password);
-    res.status(200).json({status: 'ok', token: token});
+      res.status(200).json({status: 'ok', token: token});
     } catch(error) {
       return next(error);
     }
