db6550334dcde41d4f508f8ec2d41424ad1cd2bf
Roban87 <pap.zoltan87@gmail.com>
2020-10-16 18:40:56 +0200

    refreshed services index.js

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,7 +1,5 @@
 export * from './helloService';
 export * from './buildingsService';
 export * from './registrationValidator';
-<<<<<<< HEAD
-=======
 export * from './resourceService';
->>>>>>> 94e6912... Mb 26 (#4)
+export * from './loginService';
