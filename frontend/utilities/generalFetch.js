3f527e0dbdb8e6fe4b1835dc8fdc57c5f70054e0
Roban87 <pap.zoltan87@gmail.com>
2020-10-20 13:56:09 +0200

    added redirection to general fetch function

--- frontend/utilities/generalFetch.js
+++ frontend/utilities/generalFetch.js
@@ -1,15 +1,22 @@
 
+export const fetchDataGeneral = (route, kingdomId, token, data) => {
+  const fetchedData = await fetch(`${process.env.REACT_APP_API_PATH}/${route}/${kingdomId}`, {
+    method: method, 
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }, 
+    body: JSON.stringify(data),
+  });
 
-const fetchedData = await fetch(`${process.env.REACT_APP_API_PATH}/${route}/${kingdomId}`, {
-  method: method, 
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  }, 
-  body: JSON.stringify(data),
-});
+  const jsonData = await fetchedData.json();
 
-const jsonData = await fetchedData.json();
+  if (jsonData.status === 401) {
+    window.location.href(`${process.env.REACT_APP_API_PATH}/login/`);
+    // history.push('/login');
+    return jsonData;
+  }
 
-if (jsonData.status === 401)
+  return jsonData;
+}
