4253a5af0e8014869b81b7239f07c535ca1ec08f
kisborg <mark.borgstrom@gmail.com>
2020-10-20 16:17:45 +0200

    error handling added to buildingService getSingleBuilding

--- backend/src/services/buildingsService.js
+++ backend/src/services/buildingsService.js
@@ -7,13 +7,10 @@ export const buildingsService = {
   async getSingleBuilding(buildingId) {
     const queryResult = await buildingsRepo.getSingleBuilding(buildingId);
     const buildingData = queryResult.results[0];
-<<<<<<< HEAD
     console.log(buildingData);
     if (!buildingData) {
       throw { message: "Something went wrong...", status: 400 };
     }
-=======
->>>>>>> 8358911...  added handlers for One Building request in buildingsService and buildingsController
     return buildingData;
   }
 };
