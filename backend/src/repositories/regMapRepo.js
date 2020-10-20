1090b96d15b094c985ad471dff36739581e3867c
hamate <matehajdu2@gmail.com>
2020-10-20 10:22:39 +0200

    repo changes

--- backend/src/repositories/regMapRepo.js
+++ backend/src/repositories/regMapRepo.js
@@ -2,17 +2,18 @@ import { db } from '../data/connection';
 
 export const regMapRepo = {
   async postRegMap(kingdomId, location) {
-    try {
+    // try {
       const regMapQuery = `UPDATE kingdoms SET location=? WHERE id=?`;
       const queryData = await db.query(regMapQuery, [location, kingdomId]);
       console.log('inserted');
-      const resQuery = await `SELECT k.*, b.*, u.*
-    FROM kingdoms k, buildings b, users u
-    WHERE k.id = ? AND k.id = b.kingdom_id AND k.user_id = u.id;`;
+    //   const resQuery = await `SELECT k.*, b.*, u.*
+    // FROM kingdoms k, buildings b, users u
+    // WHERE k.id = ? AND k.id = b.kingdom_id AND k.user_id = u.id;`;
+      const resQuery = `SELECT * FROM kingdoms JOIN buildings on kingdoms.id=building_id `
       const resData = await db.query(resQuery, [kingdomId]);
       return resData.results;
-    } catch (error) {
-      throw { status: 500, message: 'Internal server error' };
-    }
+    // } catch (error) {
+    //   throw { status: 500, message: 'Internal server error' };
+    // }
   },
 };
