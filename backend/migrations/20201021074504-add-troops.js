1aca42c6acc19a81a302d195fea978ef8394e5a3
hamate <36328855+hamate@users.noreply.github.com>
2020-10-21 11:28:17 +0200

    troops migration file added (#16)
    * troop migration file added
    
    * kingdom_id set as FK
    
    * FK modified

---
+++ backend/migrations/20201021074504-add-troops.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('troops', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    level: 'int',
+    hp: 'int',
+    attack: 'int',
+    defence: 'int',
+    started_at: 'timestamp',
+    finished_at: 'timestamp',
+    kingdom_id: {
+      type: 'int',
+      notNull: true,
+      foreignKey: {
+        name: 'id',
+        table: 'kingdoms',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT',
+        },
+      },
+    },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('troops');
+};
+
+exports._meta = {
+  version: 1,
+};
