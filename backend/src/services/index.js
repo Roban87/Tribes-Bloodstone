f2d9396b6524002f386f5c179923df917f6f7811
KornelKovats <kovats.kornel95@gmail.com>
2020-10-22 13:02:39 +0200

    changed services index.js file

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -4,7 +4,4 @@ export * from './registerMapService';
 export * from './registrationValidator';
 export * from './resourceService';
 export * from './loginService';
-<<<<<<< HEAD
-=======
 export * from './kingdomUpdateNameService';
->>>>>>> eb8f83f... added new controller and service
