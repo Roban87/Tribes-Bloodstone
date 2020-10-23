6a874f89dfa9e42b73fb8de6de48e4e40f426d40
KornelKovats <kovats.kornel95@gmail.com>
2020-10-23 19:11:58 +0200

    submit event listener ready needs testing

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -3,14 +3,34 @@ import { useState } from 'react';
 import './Settings.css';
 
 function Settings() {
-    const [kingdomname, setKingdomName] = useState('');
+  const [kingdomname, setKingdomName] = useState('');
+  const path = process.env.REACT_APP_API_PATH;
 
-    const onKingdomNameChange = (e) => {
-        setKingdomName(e.target.value);
+  const onKingdomNameChange = e => {
+    setKingdomName(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fetchData = {
+      name: kingdomname
     }
+    let token = localStorage.getItem('token');
+    let response = await fetch(`${path}/settings`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'accepts': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(fetchData),
+    });
+    let data = await response.json();
+    console.log(data);
+    
+  };
   return (
     <div>
-      <form className="submit-container">
+      <form className="submit-container" onSubmit={handleSubmit}>
         <h2>Kingdom Settings</h2>
         <input
           type="text"
@@ -20,7 +40,9 @@ function Settings() {
           onChange={onKingdomNameChange}
         />
 
-        <button type="submit" className="submit-button">UPDATE SETTINGS</button>
+        <button type="submit" className="submit-button">
+          UPDATE SETTINGS
+        </button>
       </form>
     </div>
   );
