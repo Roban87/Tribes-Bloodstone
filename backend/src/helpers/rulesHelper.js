96aa3980b779bdb3b886c3f34d4b8c040b9df020
kisborg <mark.borgstrom@gmail.com>
2020-10-26 13:44:05 +0100

    upgradeBuilding service

--- backend/src/helpers/rulesHelper.js
+++ backend/src/helpers/rulesHelper.js
@@ -1,21 +1,33 @@
-export const rules = level => ({
+export const setRules = level => ({
   troops: {
-    price: 10 * level,
-    time: 1 + (0.5 * level),
-    food: 1,
+    price: 25 + (25 * level),
+    time: 30 + (30 * level),
+    food: 5 + (5 * level),
+    attack: 10 + (10 * level),
+    defense: 5 + (5 * level),
   },
-  buildings: {
-    gold: 100 * level,
-    time: 1 + (0.5 * level),
-    townhall: {
-      maxLevel: 10,
-      storageLimit: 20,
-    },
-    mine: {
-      generation: 10 + (5 * level),
-    },
-    farm: {
-      generation: 10 + (5 * level),
-    },
-  }
+  townhall: {
+    maxLevel: 10,
+    storageLimit: 20,
+    price: 200 * level,
+    time: 120 + (60 * level),
+    hp: 200 + (200 * level),
+  },
+  farm: {
+    price: 100 + (100 * level),
+    time: 60 + (60 * level),
+    generation: 5 + (5 * level),
+    hp: 100 + (100 * level),
+  },
+  mine: {
+    price: 100 + (100 * level),
+    time: 60 + (60 * level),
+    generation: 5 + (5 * level),
+    hp: 100 + (100 * level),
+  },
+  academy: {
+    price: 150 + 100 * level,
+    time: 90 + 60* level,
+    hp: 150 + (150 * level),
+  },
 });
