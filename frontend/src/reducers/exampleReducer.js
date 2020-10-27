e8139ef7baddcd092a69a5a3a9f2c6002ffe2653
Roban87 <pap.zoltan87@gmail.com>
2020-10-27 09:41:47 +0100

    removed switch/case example from reducers

--- frontend/src/reducers/exampleReducer.js
+++ frontend/src/reducers/exampleReducer.js
@@ -4,30 +4,6 @@ const initialState = {
   error: ''
 }
 
-//with switch-cases
-const exampleReducer = (state = initialState, action) => {
-  switch (action.type){
-    case ADD_TO_COUNT:
-      return {
-        ...state,
-        counts: [...state.counts, action.count]
-      };
-    case GET_SUM:
-      return {
-        ...state,
-        sum: count.reduce((a, b) => { return a + b})
-      };
-    case SET_ERROR:
-      return {
-        ...state,
-        error: action.error
-      };
-    //optional, but on the redux page it is recommended to use
-    default:
-      return state;
-  }
-};
-
 //with if statements
 const exampleReducer = (state = initialState, action) => {
   if (action.type === 'ADD_TO_COUNT' ) {
