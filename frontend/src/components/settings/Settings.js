cdc937c77341d337c13df3ef0ca3423c53c802bc
KornelKovats <kovats.kornel95@gmail.com>
2020-10-27 13:57:28 +0100

    need to store states in redux store

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -2,30 +2,39 @@ import React from 'react';
 import { useState } from 'react';
 import './Settings.css';
 import { fetchDataGeneral } from '../../utilities/generalFetch';
+import { useHistory } from 'react-router-dom';
 
 function Settings() {
   const [kingdomname, setKingdomName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
 
   const onKingdomNameChange = e => {
     setKingdomName(e.target.value);
+    setErrorMessage('');
   };
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const endpoint = '/settings';
+    const endpoint = '/kingdom';
     const method = 'PUT';
     const fetchData = {
-      name: kingdomname 
-    }
+      name: kingdomname,
+    };
     try {
       let settingsData = await fetchDataGeneral(endpoint, method, fetchData);
-      console.log(settingsData);
+      if (kingdomname.length >= 3) {
+        console.log(settingsData);
+        history.push('/main');
+      } else {
+        setErrorMessage('Needs minimum 3 character for kingdomname');
+      }
     } catch (error) {
       console.log(error);
+      
     }
-    
   };
   return (
-    <div>
+    <div className="settings-container">
       <form className="submit-container" onSubmit={handleSubmit}>
         <h2>Kingdom Settings</h2>
         <input
@@ -35,6 +44,12 @@ function Settings() {
           placeholder="Kingdom's name"
           onChange={onKingdomNameChange}
         />
+        {errorMessage && (
+          <div className="error-message">
+            <p>{errorMessage}</p>
+            <i className="fas fa-exclamation-triangle"></i>
+          </div>
+        )}
 
         <button type="submit" className="submit-button">
           UPDATE SETTINGS
