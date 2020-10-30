d875e4ac93e0bb77769b5c5492e4f5d1f279aa17
hamate <36328855+hamate@users.noreply.github.com>
2020-10-30 14:31:47 +0100

    rebasing (#35)

--- backend/src/controllers/tests/troopsController.test.js
+++ backend/src/controllers/tests/troopsController.test.js
@@ -1,10 +1,97 @@
 import request from 'supertest';
 import jwt from 'jsonwebtoken';
-import { troopsService, resourceService } from '../../services';
 import app from '../../app';
+import {
+  buildingsRepo,
+  kingdomRepo,
+  resourceRepo,
+  troopsRepo,
+} from '../../repositories';
+import { troopsService, resourceService } from '../../services';
 import config from '../../config';
 
-const token = jwt.sign({ id: 1, kingdomId: 1 }, config.secret);
+const token = jwt.sign('payload', config.secret);
+
+const kingdomDB = [
+  {
+    id: 1,
+    kingdomname: 'futys',
+    population: null,
+    location: null,
+    user_id: 1,
+  },
+];
+
+const buildingsDB = [
+  {
+    id: 1,
+    type: 'gold',
+    level: 1,
+    hp: 5,
+    started_at: null,
+    finished_at: null,
+  },
+  {
+    id: 2,
+    type: 'silver',
+    level: 2,
+    hp: 222,
+    started_at: null,
+    finished_at: null,
+  },
+];
+const resourcesDB = [
+  {
+    type: 'nothing',
+    amount: 5,
+    generation: 1,
+    updatedAt: 1603281789,
+  },
+  {
+    type: 'somethin',
+    amount: 2,
+    generation: 1,
+    updatedAt: 1603284687,
+  },
+];
+const troopsDB = {
+  troops: {
+    id: 1,
+    level: 1,
+    hp: 1,
+    attack: 1,
+    defence: 1,
+    started_at: null,
+    finished_at: null,
+  },
+};
+
+describe('GET /api/kingdom/troops', () => {
+  it('responds with json containing the troops of the kingdom', (done) => {
+    const spyKingdom = jest.spyOn(kingdomRepo, 'getKingdom');
+    spyKingdom.mockReturnValue({
+      results: kingdomDB,
+      fields: 'somedata',
+    });
+    const spyBuildings = jest.spyOn(buildingsRepo, 'getBuildings');
+    spyBuildings.mockReturnValue(buildingsDB);
+    const spyResources = jest.spyOn(resourceRepo, 'getResources');
+    spyResources.mockReturnValue({ results: resourcesDB, fields: 'somedata' });
+    const spyTroops = jest.spyOn(troopsRepo, 'getTroops');
+    spyTroops.mockReturnValue(troopsDB.troops);
+    request(app)
+      .get('/api/kingdom/troops')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toEqual(troopsDB);
+        return done();
+      });
+  });
+});
 
 describe('POST api/kingdom/troops', () => {
   it('returns a JSON object of the newly added troop', (done) => {
