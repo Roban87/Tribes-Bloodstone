7957aa6ef5ec8c20b62af7fb08e07890497a3cc7
matecserven <mate.cserven@gmail.com>
2020-10-30 15:14:28 +0100

    fix merge error

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -28,17 +28,16 @@ router.post('/register/map/:kingdomId', registerMapController.post);
 router.use(authHandler);
 router.use(updateResources);
 
-router.get('/kingdom/buildings/', buildingsController.get);
-router.get('/kingdom/buildings/:buildingId', buildingsController.getBuilding);
-router.post('/kingdom/buildings', buildingsController.post);
-router.put('/kingdom/buildings/:buildingId', buildingsController.put);
-router.get('/kingdom/resource/:kingdomId', resourceController.get);
-router.post('/kingdom/troops', troopsController.post);
-router.get('/kingdom/map/', registerMapController.get);
-router.get('/kingdom/troops', troopsController.get);
-
 router.put('/kingdom', kingdomController.put);
-router.post('/register/map/:kingdomId', registerMapController.post);
+router.get('/kingdom/map/', registerMapController.get);
+router.get('/kingdom/buildings/', buildingsController.get);
+router.post('/kingdom/buildings', buildingsController.post);
+router.get('/kingdom/resource/:kingdomId', resourceController.get);
+router.get('/kingdom/buildings/:buildingId', buildingsController.getBuilding);
+router.put('/kingdom/buildings/:buildingId', buildingsController.put);
+router.get('/kingdom/troops', troopsController.get);
+router.post('/kingdom/troops', troopsController.post);
+
 router.get('/rules', rulesController.get);
 
 export default router;
