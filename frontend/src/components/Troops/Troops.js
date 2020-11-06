7898ebe7f6e9b2d4c83de638a24605e6c0bda87a
hamate <36328855+hamate@users.noreply.github.com>
2020-11-06 09:53:01 +0100

    MB 51 (#43)
    * troops basics
    
    troops container implemented, redux connected
    
    no troops returned
    
    total attack and defence stats working
    
    troops frontend working
    
    troop listing works with useState
    
    * prop types fixed
    
    * actions fixed

--- frontend/src/components/Troops/Troops.js
+++ frontend/src/components/Troops/Troops.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import troopImage from '../../assets/icons/TroopsPage.png';
 
-function Troops() {
+function Troops(props) {
+  const { level, count } = props;
   return (
     <div className="troops-container">
-      <div className="total-info">Troops data</div>
-      <div className="troops-list">Troops list</div>
+      <div className="troops-list">
+        <img className="small-troop-image" src={troopImage} alt="Troops" />
+        <p className="troop-levels">
+          <strong>{count}</strong>
+          &nbsp;Troops level&nbsp;
+          {level}
+        </p>
+      </div>
     </div>
   );
 }
+
+Troops.propTypes = {
+  level: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+};
 
 export default Troops;
