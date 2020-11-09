f7f718a75e4dc572fa0395c08b85de787eefc49a
KornelKovats <kovats.kornel95@gmail.com>
2020-11-09 15:29:12 +0100

    modified ConstructionLog

--- frontend/src/components/Construction/ConstructionLog.js
+++ frontend/src/components/Construction/ConstructionLog.js
@@ -1,36 +1,29 @@
-import React, { useEffect } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Construction from './Construction';
 import ConstructionTitle from './ContstructionTitle';
 
-import { removeAddBuildingError } from '../../actions/errorActions';
-import { setBuildingsAsync } from '../../actions/buildingsActions';
-
 function ConstructionLog(props) {
-  const { buildings } = props;
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setBuildingsAsync());
-    return () => {
-      dispatch(removeAddBuildingError());
-    };
-  }, [dispatch]);
+  const { buildings, troops } = props;
 
   const logs = buildings
+    .concat(troops)
+    .sort((a, b) => a.finished_at - b.finished_at)
     .filter((element) => {
-      if (Date.parse(element.finishedAt) > new Date().getTime() - 30 * 10 * 1000) {
+      if (
+        Date.parse(element.finishedAt) >
+        new Date().getTime() - 30 * 10 * 1000
+      ) {
         return element;
       }
     })
     .map((element, index) => {
       return <Construction key={index} element={element} />;
     })
-    .sort((a, b) => a.finished_at - b.finished_at)
     .reverse()
-    .slice(0, 6);
+    .slice(0, 6)
+
   return (
     <div>
       <ConstructionTitle />
@@ -40,8 +33,8 @@ function ConstructionLog(props) {
 }
 const mapStateToProps = (state) => ({
   buildings: state.buildings.buildings,
-  setBuildingsError: state.error.buildingsError,
-  addBuildingError: state.error.addBuildingError,
+  resources: state.resources.resources,
+  troops: state.troops.troops,
 });
 ConstructionLog.propTypes = {
   buildings: PropTypes.arrayOf(
@@ -54,13 +47,20 @@ ConstructionLog.propTypes = {
       finishedAt: PropTypes.string,
     })
   ),
-  setBuildingsError: PropTypes.string,
-  addBuildingError: PropTypes.string,
+  troops: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      level: PropTypes.number,
+      hp: PropTypes.number,
+      attack: PropTypes.number,
+      defence: PropTypes.number,
+      started_at: PropTypes.string,
+      finished_at: PropTypes.string,
+    })
+  ),
 };
-
 ConstructionLog.defaultProps = {
   buildings: [],
-  setBuildingsError: '',
-  addBuildingError: '',
+  troops: [],
 };
 export default connect(mapStateToProps)(ConstructionLog);
