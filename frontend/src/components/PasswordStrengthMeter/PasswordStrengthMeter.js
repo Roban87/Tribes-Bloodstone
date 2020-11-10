98c2e5a9ae080a539a081df1262ea706625449fd
KornelKovats <kovats.kornel95@gmail.com>
2020-11-10 13:54:28 +0100

    added

---
+++ frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './PasswordStrengthMeter.css';
+
+class PasswordStrengthMeter extends Component {
+  render() {
+    return (
+      <div className="password-strength-meter">
+        I'm a password strength meter
+      </div>
+    );
+  }
+}
+
+export default PasswordStrengthMeter;
