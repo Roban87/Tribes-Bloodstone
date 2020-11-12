ea240217c9bcc45500b94eab18027d54c57e9c2c
Márk Borgström <64068936+kisborg@users.noreply.github.com>
2020-11-12 15:52:04 +0100

    fixed error handling in repo files (#58)

--- backend/src/repositories/resourceRepo.js
+++ backend/src/repositories/resourceRepo.js
@@ -4,12 +4,20 @@ export const resourceRepo = {
 
   async getResources(kingdomId) {
     const sqlGetResources = 'SELECT type, amount, generation, UNIX_TIMESTAMP(updatedAt) as updatedAt FROM resources WHERE kingdom_id = ?;';
-    return await db.query(sqlGetResources, kingdomId);
+    try {
+      return await db.query(sqlGetResources, kingdomId);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
 
   async insertResource(kingdomId, type, amount) {
     const sqlInsertResource = 'INSERT INTO resources(kingdom_id, type, amount) VALUES(?,?,?);';
-    return await db.query(sqlInsertResource, [kingdomId, type, amount]);
+    try {
+      return await db.query(sqlInsertResource, [kingdomId, type, amount]);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
 
   async updateResources(kingdomId) {
@@ -19,26 +27,34 @@ export const resourceRepo = {
                     ELSE amount + (FLOOR(((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(updatedAt)) / 60 )) * generation ) 
                     END
                     WHERE kingdom_id=?;`;
-    return await db.query(sql, kingdomId);
+    try {
+      return await db.query(sql, kingdomId);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
 
   async getGoldAmount(kingdomId) {
     const sqlGetGoldAmount = 'SELECT amount FROM resources WHERE type = \'gold\' AND kingdom_id = ?;';
-    const getGoldAmount = await db.query(sqlGetGoldAmount, [kingdomId]);
-    return getGoldAmount.results;
+    try {
+      const getGoldAmount = await db.query(sqlGetGoldAmount, [kingdomId]);
+      return getGoldAmount.results;
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
 
   async handlePurchase(kingdomId, price) {
+    const sql = `
+      UPDATE resources
+      SET amount = amount - ?
+      WHERE kingdom_id = ?
+      AND type = 'gold';  
+    `;
     try {
-      const sql = `
-        UPDATE resources
-        SET amount = amount - ?
-        WHERE kingdom_id = ?
-        AND type = 'gold';  
-      `;
       return await db.query(sql, [price, kingdomId]);
     } catch (err) {
-      throw { status: 500, message: 'Internal server error' };
+      throw { status: 500, message: err.sqlMessage };
     }
   },
 
@@ -52,7 +68,7 @@ export const resourceRepo = {
       `;
       return await db.query(sql, [increment, kingdomId, resourceType]);
     } catch (err) {
-      throw { status: 500, message: 'Internal server error' };
+      throw { status: 500, message: err.sqlMessage };
     }
   },
   async updateResourceAfterBattle(kingdomId, goldAmount, foodAmount) {
@@ -60,7 +76,7 @@ export const resourceRepo = {
     try {
       return await db.query(sqlQuery, [goldAmount, foodAmount, kingdomId]);
     } catch (err) {
-      throw { status: 500, message: 'Internal server error' };
+      throw { status: 500, message: err.sqlMessage };
     }
   },
 };
--- backend/src/repositories/userRepo.js
+++ backend/src/repositories/userRepo.js
@@ -3,26 +3,38 @@ import { db } from '../data/connection';
 export const userRepo = {
   async getUserByUsername(username) {
     const sqlQuery = 'SELECT * FROM users WHERE username=?;';
-    return await db.query(sqlQuery, [username]);
+    try {
+      return await db.query(sqlQuery, [username]);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
   async insertNewUser(username, password) {
     const sqlQueryInsertUser = 'INSERT INTO users(username, password) VALUES(?,?);';
-    return await db.query(sqlQueryInsertUser, [username, password]);
+    try {
+      return await db.query(sqlQueryInsertUser, [username, password]);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
   async selectUserWithKingdom(username) {
     const sqlQuerySelect = 'SELECT users.id, users.username, kingdoms.id as kingdom_id FROM users JOIN kingdoms ON users.id=kingdoms.user_id WHERE username=?;';
-    return await db.query(sqlQuerySelect, [username]);
+    try {
+      return await db.query(sqlQuerySelect, [username]);
+    } catch (err) {
+      throw { status: 500, message: err.sqlMessage };
+    }
   },
   async getUser(username) {
+    const sql = `SELECT users.id, password, kingdoms.id AS kingdomId FROM users
+      JOIN kingdoms ON users.id = kingdoms.user_id
+      WHERE username = ?
+      ORDER BY kingdoms.id ASC
+      LIMIT 1`;
     try {
-      const sql = `SELECT users.id, password, kingdoms.id AS kingdomId FROM users
-        JOIN kingdoms ON users.id = kingdoms.user_id
-        WHERE username = ?
-        ORDER BY kingdoms.id ASC
-        LIMIT 1`;
       return await db.query(sql, username);
     } catch (err) {
-      throw { status: 500, message: 'Internal server error' };
+      throw { status: 500, message: err.sqlMessage };
     }
   },
   async getUserKingdomData(userId, kingdomId) {
@@ -37,7 +49,7 @@ export const userRepo = {
       const userQuery = await db.query(sqlQuery, [kingdomId, userId]);
       return userQuery.results[0];
     } catch (err) {
-      throw { status: 500, message: 'Internal server error' };
+      throw { status: 500, message: err.sqlMessage };
     }
   },
 };
